91f9d3242a9d925e3dc41678935c8eb7a123d855
anticbojana15 <anticbojana15@gmail.com>
2019-03-14 14:13:59 +0100

    Connection created

---
+++ core/database/Connection.php
@@ -0,0 +1,30 @@
+<?php 
+
+	class Connection {
+
+		public static function connect ($config){
+
+			try {
+
+				return new PDO(
+
+					$config['connection'].';dbname='.$config['name'],
+					$config['username'],
+					$config['password'],
+					$config['options']
+				);
+				 
+			} catch (Exception $e) {
+
+				die('Connection failed');
+				
+			}
+
+		}
+	}
+
+
+
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+
+require 'core/includes.php';
+
+ ?>
